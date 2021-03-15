052f8be7c67b664ca79684edc061bc51f256039b
Fabio Maggi <fabio.maggi@alfaconcursos.com.br>
2021-03-15 11:14:06 -0300

    173. Componente Comentários

--- App.js
+++ App.js
@@ -6,10 +6,20 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+
+    const comments = [{
+      nickname: 'Joana Elena da Silva',
+      comment: 'Excelente Foto!'
+    },
+    {
+      nickname: 'Rafael Gustavo Pereira',
+      comment: 'Muito ruim! Faço melhor...'
+    }]
+
     return (
       <View style={{ flex: 1 }}>
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')} comments={comments}/>
       </View>
     )
   }
